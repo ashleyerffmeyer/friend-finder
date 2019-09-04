97690c0501234dc71264527dfd973ed2bf5f49a4
ashleyerffmeyer <erffmea@gmail.com>
2019-09-04 13:05:25 -0500

    adding js to server.js file

--- server.js
+++ server.js
@@ -6,20 +6,17 @@ var bodyParser = require('body-parser');
 // Establishing the PORT
 var PORT = process.env.PORT || 8080;
 
-// create application/json parser
-var jsonParser = bodyParser.json()
-
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({ type: 'application/*+json' }));
 
 // parse some custom thing into a Buffer
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
 
 // parse an HTML body into a string
-app.use(bodyParser.text({ type: 'text/html' }))
+app.use(bodyParser.text({ type: 'text/html' }));
 
 // Including necessary routes for server
 require("./app/routing/apiRoutes.js")(app);
