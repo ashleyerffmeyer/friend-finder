873a0e167c225c0fa2537051174ad68989c16bc3
ashleyerffmeyer <erffmea@gmail.com>
2019-09-05 10:45:28 -0500

    updated friends.js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,123 +6,123 @@ var friends = [
         photo: "https://www.sanrio.com/media/W1siZiIsIjIwMTYvMDYvMTMvMTQvMTEvNDAvMTQvY2hhcmFjdGVyX2Jhbm5lcl9oZWxsb2tpdHR5LnBuZyJdXQ/character_banner_hellokitty.png?sha=95006e8644727395",
         scores: [
             "5",
+            "2",
+            "5",
             "1",
             "1",
             "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "5"
+            "4",
+            "3",
+            "5",
+            "2"
         ]
     },
     {
         name: "Kerropi",
         photo: "https://www.sanrio.com/media/W1siZiIsIjIwMTYvMDYvMTUvMjEvMzEvMDQvMjg4L2NoYXJhY3RlcnNfaGVhZGVyX2Jhbm5lcnNfaW1nX2tyLnBuZyJdXQ/characters-header-banners-img-kr.png?sha=55881c100af63b27",
         scores: [
+            "2",
             "5",
             "1",
             "1",
+            "3",
+            "2",
+            "4",
+            "4",
             "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "5"
+            "2"
         ]
     },
     {
         name: "Batz Maru",
         photo: "https://www.sanrio.com/media/W1siZiIsIjIwMTYvMDYvMTYvMTEvMzkvMTQvMy9jaGFyYWN0ZXJzX2hlYWRlcl9iYW5uZXJzX2ltZ194by5wbmciXV0/characters-header-banners-img-xo.png?sha=057433f7b88c866a",
         scores: [
+            "1",
+            "4",
+            "1",
+            "3",
             "5",
+            "2",
             "1",
+            "2",
             "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "5"
+            "4"
         ]
     },
     {
         name: "Aggretsuko",
         photo: "https://www.sanrio.com/media/W1siZiIsIjIwMTgvMTAvMzAvMjEvMzIvMzQvMjYzL2NoYXJhY3RlcnNfaGVhZGVyX2Jhbm5lcnNfaW1nX2FnZy5qcGciXV0/characters-header-banners-img-agg.jpg?sha=b8be75815d72c8c8",
         scores: [
+            "3",
+            "4",
+            "1",
             "5",
+            "4",
+            "4",
             "1",
             "1",
             "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "5"
+            "1"
         ]
     },
     {
         name: "My Melody",
         photo: "https://www.sanrio.com/media/W1siZiIsIjIwMTYvMDYvMTUvMjEvMzEvMDUvNTk3L2NoYXJhY3RlcnNfaGVhZGVyX2Jhbm5lcnNfaW1nX21tLnBuZyJdXQ/characters-header-banners-img-mm.png?sha=a04ecd74d46c70e6",
         scores: [
+            "3",
+            "4",
+            "1",
+            "1",
+            "1",
+            "2",
             "5",
+            "2",
             "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "5"
+            "1"
         ]
     },
     {
         name: "Pochacco",
         photo: "https://www.sanrio.com/media/W1siZiIsIjIwMTYvMDYvMTUvMjEvMzEvMDUvOS9jaGFyYWN0ZXJzX2hlYWRlcl9iYW5uZXJzX2ltZ19wYy5wbmciXV0/characters-header-banners-img-pc.png?sha=c5d89a2486f6ea88",
         scores: [
+            "3",
+            "2",
+            "1",
+            "1",
+            "3",
+            "1",
+            "3",
             "5",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "5"
+            "4",
+            "1"
         ]
     },
     {
         name: "Chococat",
         photo: "https://www.sanrio.com/media/W1siZiIsIjIwMTYvMDYvMTUvMjEvMzEvMDMvNTQyL2NoYXJhY3RlcnNfaGVhZGVyX2Jhbm5lcnNfaW1nX2NvLnBuZyJdXQ/characters-header-banners-img-co.png?sha=000506095b48d8e5",
         scores: [
+            "1",
+            "2",
+            "1",
+            "1",
+            "2",
             "5",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "1",
-            "5"
+            "3",
+            "2",
+            "2",
+            "4"
         ]
     },
     {
         name: "Gudetama",
         photo: "https://www.sanrio.com/media/W1siZiIsIjIwMTgvMTAvMzAvMTgvNTMvMDEvODMxL2NoYXJhY3RlcnNfaGVhZGVyX2Jhbm5lcnNfaW1nX2d1LmpwZyJdXQ/characters-header-banners-img-gu.jpg?sha=0ed05b8c9654b49c",
         scores: [
-            "5",
+            "2",
             "1",
             "1",
-            "1",
-            "1",
-            "1",
+            "3",
+            "4",
+            "2",
             "1",
             "1",
             "1",
